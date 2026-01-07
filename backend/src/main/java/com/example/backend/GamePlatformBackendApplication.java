8b2508d24d07cedd40e5b123f776c1be7f680e84
loziamina <90446832+loziamina@users.noreply.github.com>
2026-01-07 09:50:12 +0100

    init projet

---
+++ backend/src/main/java/com/example/backend/GamePlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamePlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamePlatformBackendApplication.class, args);
+	}
+
+}
