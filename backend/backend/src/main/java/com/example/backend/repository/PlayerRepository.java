1f4b5bc9e5af9bbbaef07be00a2162b7a2f4f6a7
loziamina <90446832+loziamina@users.noreply.github.com>
2026-01-09 00:32:31 +0100

    Delete backend/backend directory

--- backend/backend/src/main/java/com/example/backend/repository/PlayerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.entity.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<Player, Long> {
-}
